fe9b53c45e39687e48b0945f27c1274029ad0363
Mikhail <mishajudoist@gmail.com>
2022-06-07 19:28:57 +0300

    done 100% coverage

--- students_test.go
+++ students_test.go
@@ -1,8 +1,11 @@
 package coverage
 
 import (
+	"errors"
+	"fmt"
 	"os"
 	"sort"
+	"strconv"
 	"testing"
 	"time"
 )
@@ -172,3 +175,144 @@ func TestLess(t *testing.T) {
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////
+
+func sliceMatch(a, b []int) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i, _ := range a {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+	return true
+}
+
+func TestNew(t *testing.T) {
+
+	tData := []struct {
+		str      string
+		Expected *Matrix
+		err      error
+	}{
+		{
+			str:      "1 2\n3 4\n5 6",
+			Expected: &Matrix{3, 2, []int{1, 2, 3, 4, 5, 6}},
+			err:      nil,
+		},
+		{
+			str:      "1 2\n3 4\n5",
+			Expected: nil,
+			err:      fmt.Errorf("Rows need to be the same length"),
+		},
+		{
+			str:      "1 2\n3 4\n5 a",
+			Expected: nil,
+			err:      strconv.ErrSyntax,
+		},
+		{
+			str:      "4 2\n3 4",
+			Expected: &Matrix{2, 2, []int{4, 2, 3, 4}},
+			err:      nil,
+		},
+	}
+	for k, v := range tData {
+		got, err := New(v.str)
+		if err != nil && !errors.As(err, &v.err) {
+			t.Errorf("[%d] error happend while not expected: %s", k, err.Error())
+		}
+		if got != nil && (got.rows != v.Expected.rows || got.cols != v.Expected.cols || !sliceMatch(got.data, v.Expected.data)) {
+			t.Errorf("[%d] expected: %v, got %v", k, v.Expected, got)
+		}
+	}
+}
+
+func TestRows(t *testing.T) {
+	tData := []struct {
+		matrix   Matrix
+		Expected [][]int
+	}{
+		{
+			Matrix{3, 2, []int{1, 2, 3, 4, 5, 6}},
+			[][]int{{1, 2}, {3, 4}, {5, 6}},
+		},
+		{
+			Matrix{2, 1, []int{1, 2}},
+			[][]int{{1}, {2}},
+		},
+	}
+	for k, v := range tData {
+		got := v.matrix.Rows()
+		for i, elem := range got {
+			if !sliceMatch(elem, v.Expected[i]) {
+				t.Errorf("[%d] expected: %v, got %v", k, v.Expected, got)
+				break
+			}
+		}
+	}
+}
+
+func TestCols(t *testing.T) {
+	tData := []struct {
+		matrix   Matrix
+		Expected [][]int
+	}{
+		{
+			Matrix{3, 2, []int{1, 2, 3, 4, 5, 6}},
+			[][]int{{1, 3, 5}, {2, 4, 6}},
+		},
+		{
+			Matrix{2, 1, []int{1, 2}},
+			[][]int{{1, 2}},
+		},
+	}
+	for k, v := range tData {
+		got := v.matrix.Cols()
+		for i, elem := range got {
+			if !sliceMatch(elem, v.Expected[i]) {
+				t.Errorf("[%d] expected: %v, got %v", k, v.Expected, got)
+				break
+			}
+		}
+	}
+}
+
+func TestSet(t *testing.T) {
+	tData := []struct {
+		matrix   *Matrix
+		row      int
+		col      int
+		Expected Matrix
+	}{
+		{
+			&Matrix{3, 2, []int{1, 2, 3, 4, 5, 6}},
+			0,
+			0,
+			Matrix{3, 2, []int{2, 2, 3, 4, 5, 6}},
+		},
+		{
+			&Matrix{1, 2, []int{1, 2, 3, 4, 5, 6}},
+			0,
+			3,
+			Matrix{1, 2, []int{1, 2, 3, 2, 5, 6}},
+		},
+		{
+			&Matrix{1, 2, []int{1, 2, 3, 4, 5, 6}},
+			3,
+			0,
+			Matrix{1, 2, []int{1, 2, 3, 2, 5, 6}},
+		},
+		{
+			&Matrix{1, 1, []int{1}},
+			0,
+			0,
+			Matrix{1, 1, []int{2}},
+		},
+	}
+	for k, v := range tData {
+		got := v.matrix.Set(v.row, v.col, 2)
+		if !sliceMatch(v.matrix.data, v.Expected.data) && got {
+			t.Errorf("[%d] expected: %v, got %v", k, v.Expected, v.matrix)
+		}
+	}
+}
